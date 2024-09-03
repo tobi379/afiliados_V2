e33c0253fe65c23a4e871fecc99492d0c8c1111f
Tobi <t.pazos.carbonell@gmail.com>
2024-09-03 13:07:17 -0300

    Updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_updater::Builder::new().build()) // Agregar el plugin de actualizaciones
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
